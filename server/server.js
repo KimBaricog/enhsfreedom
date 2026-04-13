ba54b4743fb0ee44eb6f8cf0db74243e9c27ec26
Kim <kimbaricog4@gmail.com>
2026-04-13 20:30:27 +0800

    modify

--- server/server.js
+++ server/server.js
@@ -200,6 +200,8 @@ app.post("/update-profile", (req, res) => {
 });
 
 // -------------------- START SERVER --------------------
-app.listen(5000, () => {
-  console.log("Server running on http://localhost:5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
